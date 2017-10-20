5da04ec20ad6672d233a3fa5fb8195be928f0774
Matt Warren <mwarren@harding.edu>
2017-10-20 17:24:01 -0400

    add input variable and commands for set up

---
+++ netxtern.rb
@@ -0,0 +1,16 @@
+input = %q(
+	https://google.com
+	/search
+	query
+	term/kittens
+	/calendar/today
+	events/mine
+	https://reddit.com
+	r/kittens
+	BACK
+	FORWARD
+	new
+)
+
+commands = [ "BACK", "FORWARD" ]
+
