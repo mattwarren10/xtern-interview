e2bbf2eb7c2d0ebe4e4c756ee0d32c5f32f1b56e
Matt Warren <mwarren@harding.edu>
2017-10-20 23:09:26 -0400

    add output method to output the absolute urls

--- netxtern.rb
+++ netxtern.rb
@@ -1,44 +1,62 @@
 
-commands = [ "BACK", "FORWARD" ]
 
-abs_rel_path = /[A-Za-z0-9]+/
+
+ABS_URL = /https:\/\/[A-Za-z0-9]+\.com/
 
 def convert(input)
-	abs_url = /https:\/\/[A-Za-z0-9]+\.com/
 	arr = input.split("\n")
 	abs_url_indices = []
 	arr.each_with_index do |str, i|
-		if str.match(abs_url)
+		if str.match(ABS_URL)
 			abs_url_indices << i			
 		end		
 	end
 	[arr, abs_url_indices]
 end
 
-def output(arr)
+def output(input)
+	arr = convert(input)
 	routes = arr[0]
-	indices = arr[1]
+	abs_url_indices = arr[1]
 	arr = []
 	routes.each_with_index do |str, i|
-		
+		if abs_url_indices.include?(i)
+			arr << str			
+		else
+			# add new commands to this array, ie: "REFRESH", "STOP"
+			commands = [ "BACK", "FORWARD" ]			
+			if str == ""				
+				next
+			elsif commands.include?(str)
+			else
+				str[0] != "/" ? str.prepend("/") : str
+				abs_url_indices.each do |num|						
+					if i > num
+						str = routes[num] + str		
+					end
+				end				
+				arr << str
+			end
+		end
 	end
+	puts arr
 end
 
 
 
-
 input = %q(
-	https://google.com
-	/search
-	query
-	term/kittens
-	/calendar/today
-	events/mine
-	https://reddit.com
-	r/kittens
-	BACK
-	FORWARD
-	new
+https://google.com
+/search
+query
+term/kittens
+/calendar/today
+events/mine
+https://reddit.com
+r/kittens
+BACK
+FORWARD
+new
 )
 
-convert(input)
+
+output(input)
